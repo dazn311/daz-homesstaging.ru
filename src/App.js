9e9ffa8879fd8142ac55d98e2b43f1c0527b5ed3
Alexander Ryzhenkov <alex2505@bk.ru>
2025-11-12 10:06:35 +0300

    init project [251112-1006];

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Daz Home page for public.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// import logo from './logo.svg';
+//<img src={logo} className="App-logo" alt="logo" />
